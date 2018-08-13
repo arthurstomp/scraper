336a02c94e9bdaf0d0864c63b25efad8e83c9613
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2018-08-12 23:00:05 -0300

    Set up mongoid gem

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module Scraper
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    Mongoid.raise_not_found_error = false
+    config.generators do |g|
+      g.orm :mongoid
+    end
+
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
